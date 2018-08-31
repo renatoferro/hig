7ba94e33acc3744207c371b956ba6b698794e553
Sean Durham <sean@sdurham.net>
2018-08-30 17:04:49 -0700

    Refactor to use dropshift more

--- packages/dropdown/src/__stories__/Dropdown.stories.js
+++ packages/dropdown/src/__stories__/Dropdown.stories.js
@@ -7,12 +7,56 @@ import stories from "./stories";
 
 const storybook = storiesOf("Forms|Dropdown", module);
 
-stories.forEach(({ description, getProps }) => {
-  storybook.add(
-    description,
-    withInfo(infoOptions)(() => {
-      const props = getProps();
-      return renderStory(props);
-    })
-  );
-});
+class Sample extends React.Component {
+  state = {
+    item: "HIG Light Theme",
+    filter: ""
+  };
+
+  handleOptionChange = optionValue => {
+    console.log("option changed: ", optionValue);
+    this.setState({ item: optionValue, filter: "" });
+  };
+
+  handleTextEntryChange = inputValue => {
+    console.log("input value changed: ", inputValue);
+    this.setState({ item: inputValue, filter: inputValue });
+  };
+
+  filterOptions = () =>
+    this.props.options.filter(o =>
+      o.toLowerCase().match(this.state.filter.toLowerCase())
+    );
+
+  render() {
+    const options = this.filterOptions();
+    return (
+      <Dropdown
+        {...this.props}
+        options={options}
+        onChange={this.handleOptionChange}
+        inputValue={
+          this.state.filter.length > 0 ? this.state.filter : this.state.item
+        }
+        onInputValueChange={this.handleTextEntryChange}
+        value={this.state.item}
+      />
+    );
+  }
+}
+
+storybook.add(
+  "default",
+  withInfo(infoOptions)(() => {
+    const props = {
+      hasTextEntry: false,
+      instructions: "Choose one HIG theme to apply to your entire app.",
+      label: "HIG Theme",
+      options: ["HIG Light Theme", "HIG Dark Blue Theme", "Matrix Theme"],
+      placeholder: "Select a theme"
+    };
+    const { children, ...otherProps } = getKnobs(props);
+
+    return <Sample {...otherProps} />;
+  })
+);
